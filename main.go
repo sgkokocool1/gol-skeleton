b0c9fd8897c5f70cc2664cea51a794dea354efa8
zhuyingfeng <yingfeng.zhu@xeniro.com>
2024-10-30 17:51:07 +0800

    fix

--- main.go
+++ main.go
@@ -26,19 +26,19 @@ func main() {
 	flag.IntVar(
 		&params.ImageWidth,
 		"w",
-		16,
+		512,
 		"Specify the width of the image. Defaults to 512.")
 
 	flag.IntVar(
 		&params.ImageHeight,
 		"h",
-		16,
+		512,
 		"Specify the height of the image. Defaults to 512.")
 
 	flag.IntVar(
 		&params.Turns,
 		"turns",
-		10,
+		100,
 		"Specify the number of turns to process. Defaults to 10000000000.")
 
 	headless := flag.Bool(
